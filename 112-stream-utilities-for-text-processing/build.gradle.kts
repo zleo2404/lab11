e6d4fd11fcd6657d1256e8e3996c5470a13de237
DanySK <danilo.pianini@gmail.com>
2024-11-05 15:32:12 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@b52e2a38a9d1593097e56803ef6b18f945a67f41 🚀

--- 112-stream-utilities-for-text-processing/build.gradle.kts
+++ 112-stream-utilities-for-text-processing/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     application
-    id("org.danilopianini.gradle-java-qa") version "1.69.0"
+    id("org.danilopianini.gradle-java-qa") version "1.74.0"
 }
 
 tasks.javadoc {
@@ -15,10 +15,10 @@ repositories {
 
 dependencies {
     // Use JUnit Jupiter API for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
 
     // Use JUnit Jupiter Engine for testing.
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
 }
 
 val mainClass: String by project
